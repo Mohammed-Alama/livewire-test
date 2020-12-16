6cbc8c3cc3afdfe2a8c5c66ab4629181ea213b88
Mohammed Alama <mohammedalsayed9688@gmail.com>
2020-12-16 04:04:46 +0200

    get play with Livecycle Hooks

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -7,18 +7,44 @@ use Livewire\Component;
 class HelloWorld extends Component
 {
     public $name;
-    public $loud = false;
-    public $greetings = 'Hello';
-    public function render()
-    {
+    public $anotherName;
+    public $count;
 
-        //it's just function to render blade view
-        //instead of passing parameters to view instance we could use public properties  
-        return view('livewire.hello-world');
-    }
-
-    public function resetName()
+    //this method called when livewire init the component
+    //what ever happened in this function called when component booted up 
+    public function mount()
     {
         $this->name = 'Alama';
+        $this->count = 0;
+    }
+
+    //this function called in any subsequent request called to component
+    public function hydrate()
+    {
+        $this->anotherName = 'Hydrate Alama';
+    }
+
+    //this function called in any subsequent request called to component
+    public function updated()
+    {
+        $this->anotherName = 'Updated anotherName';
+        $this->count = $this->count + 1;
+    }
+
+    //this function called when specific Property updated
+    public function updatedName()
+    {
+        $this->anotherName = 'name is Updated';
+    }
+
+    //this function called when updating properties in component
+    public function updating()
+    {
+        $this->anotherName = 'Updating anotherName ';
+    }
+
+    public function render()
+    {
+        return view('livewire.hello-world');
     }
 }
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,18 +1,8 @@
-{{-- <div>
-    {{-- wire model pass name to Component class and send it again with render
-    --}}
+<div>
+
     <input type="text" wire:model="name">
-    <input type="checkbox" wire:model="loud">
-    <select wire:model="greetings">
-        <option>Hallo</option>
-        <option>Good Morning</option>
-        <option>Hola</option>
-    </select>
-    {{-- <form action="#" wire:submit.prevent="resetName()"> --}}
-    {{-- you can use this way if it's just one line method to set value to property at Components --}}
-    <form action="#" wire:submit.prevent="$set('name','Alama')">
-        <button >Reset Name</button>
-    </form>
-    {{ $greetings }} {{ $name }}
-    @if ($loud) ! @endif
-</div> --}}
+
+    Hello {{$name}} {{$anotherName}}
+
+    Number Of Requests {{$count}}
+</div>
